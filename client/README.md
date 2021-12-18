db0b227bf56e67bff02e5f82ab93c39f6d2b41cb
Ashwin Kumar Uppala <53075480+ashwinexe@users.noreply.github.com>
2021-12-18 22:01:58 +0530

    boiler plate

---
+++ client/README.md
@@ -0,0 +1,56 @@
+## This is a project for Major League Hacking - Mental Hacks Hackathon
+## http://right-emotions-can-help.us/
+# Project overview 
+<img src="/src/components/img/logo-animated.gif">
+App for people with alexithymia and others who have trouble expressing and describing emotions and feelings.
+
+### Screenshots
+1. Home Page
+<img src = "screenshots/home.jpg">
+
+2. login
+<img src = "screenshots/login.jpg">
+
+3. new mood
+<img src = "screenshots/new mood.jpg">
+
+4. proud
+<img src ="screenshots/proud.jpg">
+
+# 💡 Inspiration
+
+Alexithymia is a trait (not a disorder!) that describes problems with feeling emotions. The Greek word loosely translates to "no words for emotion" because oftentimes, those with alexithymia have trouble describing and expressing their emotions. For example, they have trouble expressing happiness during a joyous occasion. Others have trouble even just understanding what they're feeling. While the condition is not well-known, it’s estimated that 1 in 10 people has it. We hope that this project helps alleviate the difficulties faced by people with alexithymia and the greater population as a whole in describing and expressing their emotions.
+
+# ⚙ What it does
+The user, after creating an account or logging in, will go through a series of multiple-choice questions asking them which word best represents how they feel. This is based on a color-coded emotion wheel often used in therapy. The emotion that they choose is logged and is then displayed on another page. This log of emotions is accompanied by a chart that visualizes positive vs negative emotions over time.
+
+# 🔧 How we built it
+We used React.js for our frontend and Firebase + Firestore for our backend. Firebase was our user authentication and hosting, while Firestore was our database.
+
+## 💪 Challenges we ran into
+We had trouble integrating Firestore and storing the user's input there.
+We were in as many as three different timezones, which made keeping in sync initially difficult.
+
+## 📌 Accomplishments that we're proud of
+We eventually delegated work effectively, which made development smooth but also made sure we didn't have merge conflicts when updating the repository. We pair programmed and collaborated seamlessly.
+We created a fully functioning, complete full-stack project in just a weekend!! 🥳
+
+## 📚 What we learned
+None of us knew how to use Firebase for auth (one only knew hosting) and none of us knew how to use Firestore. It took a lot of reading documentation and googling and collaborating to finally get these to work.
+We got better at using React through practice. Particularly, some team members learned how to do more advanced functionality.
+We learned how to maintain teamwork even when half the team was inactive during a particular time due to being in a different location.
+
+## ⏭ What's next for Alexithymia
+- We need to fix minor bugs like deployment and inaccurate GIFs.
+- We need to clean up our code to adhere to industry best practices.
+- We can improve styling and user experience.
+- We can consider other features to add.
+
+## Contributors 🙌:
+
+Every contributor's efforts and time are deeply appreciated :smile:
+
+<a href = "https://github.com/ashwinexe/WEmotion/graphs/contributors">
+  <img src = "https://contrib.rocks/image?repo=ashwinexe/WEmotion"/>
+</a>
+
---
+++ client/src/components/EmotionListDisplay.js
@@ -0,0 +1,222 @@
+import React, { useState, useContext, useEffect } from "react";
+import EmotionCard from "./EmotionCard";
+import { makeStyles } from "@material-ui/core/styles";
+import Popup from "./popup";
+import EmotionInfo from "./Emotion Info/EmotionInfo";
+import FirebaseContext from "../Firebase/context";
+import { useAuth } from "./Auth";
+// import Alert from "@material-ui/lab/Alert";
+
+const useAppStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  cards: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    fontFamily: "Open Sans",
+  },
+  back: {
+    display: "inline-block",
+    border: "none",
+    padding: "1rem 2rem",
+    margin: "32px",
+    textDecoration: "none",
+    background: "#133072",
+    color: "#FFFFFF",
+    cursor: "pointer",
+    textAlign: "center",
+    fontFamily: "Open Sans",
+  },
+  submit: {
+    border: "1px solid black",
+    borderRadius: "5px",
+  },
+});
+
+const EmotionListDisplay = () => {
+  const firestore = useContext(FirebaseContext);
+  const classes = useAppStyles();
+  const [currentEmotionActive, setCurrentEmotionActive] = useState("surprise"); //emotion card clicked on
+  const [popUpEmotion, setpopUpEmotion] = useState(null);
+  const [level2Active, setLevel2Active] = useState(false);
+  const [color, setColor] = useState();
+  const [emotionType, setEmotionType] = useState();
+  const [popUp, setpopUp] = useState(false);
+  const userAuth = useAuth();
+
+  useEffect(() => {
+    console.log(firestore, "FIRESTORE");
+    console.log(userAuth.currentUser.uid);
+
+    const db = firestore
+      .collection(`UserData/${userAuth.currentUser.uid}/user-emotions`)
+      .onSnapshot(
+        (items) => {
+          items.forEach((item) => {
+            let id = item.id;
+            let data = item.data();
+
+            console.log(data);
+          });
+        },
+        (err) => {
+          console.log(`Encountered error: ${err}`);
+        }
+      );
+  });
+
+  //simple emotions
+  const emotionList = [
+    "Surprise",
+    "Bad",
+    "Afraid",
+    "Angry",
+    "Disgust",
+    "Sad",
+    "Happy",
+  ];
+
+  //PLACEHOLDER FOR DB -second level of words, depending on what been clicked--it doesn't need to be in the db
+  const level2Words = {
+    Surprise: ["Shock", "Confusion", "Amazement", "Excitement"],
+    Happy: [
+      "Playful",
+      "Content",
+      "Curiosity",
+      "Proud",
+      "Acceptance",
+      "Powerful",
+      "Care",
+      "Trust",
+      "Hope",
+    ],
+    Sad: ["Lonely", "Vulnerable", "Despair", "Guilty", "Depression", "Hurt"],
+    Disgust: ["Disapproval", "Disdain", "Sick", "Repulsion"],
+    Angry: [
+      "Mistrust",
+      "Shame",
+      "Jealous",
+      "Mad",
+      "Irritation",
+      "Frustration",
+      "Distant",
+      "Critical",
+    ],
+    Bad: ["Boredom", "Busy", "Stress", "Tired"],
+    Afraid: ["Scared", "Anxious", "Insecure", "Weak", "Shaky", "Nervous"],
+  };
+
+  //colors for all emotions (would be in database)
+  const emotionListColor = {
+    Surprise: "#8FCDD4",
+    Bad: "#DB7558",
+    Afraid: "#ACD8CB",
+    Angry: "#F6A6B6",
+    Disgust: "#FFCF57",
+    Sad: "#DA8EC0",
+    Happy: "#FFB19B",
+  };
+
+  //updating display depending on what word has been clicked
+  const updateDisplay = (word) => {
+    setCurrentEmotionActive(word);
+    setLevel2Active(true);
+    setColor(emotionListColor[word]);
+    if (word === "Happy") {
+      setEmotionType("Positive");
+    } else {
+      setEmotionType("Negative");
+    }
+  };
+
+  let level2 = level2Active;
+  const renderBackButton = () => {
+    if (level2) {
+      return (
+        <button className={classes.back} onClick={() => setLevel2Active(false)}>
+          Go back
+        </button>
+      );
+    }
+  };
+
+  //conditionally rendering next level of words, depending on what user clicked on
+  const handleSave = () => {
+    firestore
+      .collection("UserData")
+      .doc(userAuth.currentUser.uid)
+      .collection("user-emotions")
+      .add(
+        {
+          emotion: `${popUpEmotion}`,
+          date: new Date(),
+          color,
+          emotionType,
+        },
+        { merge: true }
+      );
+  };
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.cards}>
+        {
+          //conditional if to check if word has been clicked on and update display
+          !level2Active
+            ? emotionList.map((emotion, delay) => {
+                return (
+                  <EmotionCard
+                    key={delay}
+                    emotionClicked={updateDisplay}
+                    delay={delay + 200}
+                    emotionWord={emotion}
+                    color={emotionListColor[emotion]}
+                  />
+                );
+              })
+            : //renders second level of words if level2Words state active
+              level2Words[currentEmotionActive].map((emotion, delay) => {
+                return (
+                  <EmotionCard
+                    // onClick={() => setpopUp(true)}
+                    key={delay}
+                    emotionClicked={() => {
+                      setpopUp(true);
+                      setpopUpEmotion(emotion);
+                    }}
+                    delay={delay}
+                    emotionWord={emotion}
+                    color={emotionListColor[currentEmotionActive]}
+                  />
+                );
+              })
+        }
+      </div>
+
+      <Popup trigger={popUp} setTrigger={setpopUp}>
+        <h3 style={{ textAlign: "center" }}>{`${popUpEmotion}`}</h3>
+        <EmotionInfo word={popUpEmotion} />
+        {/* button to log emotion into database*/}
+        <div style={{ textAlign: "center" }}>
+          <button
+            className={classes.submit}
+            onClick={() => {
+              handleSave();
+              setpopUp(false);
+            }}
+          >
+            Log Emotion?
+          </button>
+        </div>
+      </Popup>
+      {/*button to go back to previous words*/}
+      {renderBackButton()}
+    </div>
+  );
+};
+
+export default EmotionListDisplay;
